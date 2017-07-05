97e9790963632b02bb82b6135b3e7973fe30ea09
davemflick <davemflick@yahoo.com>
2017-07-05 09:47:16 -0400

    all finished

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.get("/", (req,res,next)=>{
 	res.render("home");
 });
 
+var storage = multer.memoryStorage();
+
+
+app.post("/", multer({storage: storage }).single('upl'), (req,res,next)=>{
+	let fileName = req.file.originalname;
+	let mimetype = req.file.mimetype;
+	let size = req.file.size;
+	let fileObj = {name: fileName, type: mimetype, size: size};
+	res.render("home", {file: fileObj});
+});
+
 app.listen(process.env.PORT || 3000, process.env.IP, ()=>{
 	console.log("Server listening");
 });
